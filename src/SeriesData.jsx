2a513a03d027618e328efc5a691a8443a83c63ed
Samarth64s <115937579+Samarth64s@users.noreply.github.com>
2023-07-13 17:52:21 +0530

    WebSeries

---
+++ src/SeriesData.jsx
@@ -0,0 +1,35 @@
+
+const SeriesData = [
+      {
+        imgsrc:"https://wallpapercave.com/w/wp12013745.jpg",
+        title :"A Amazon Prime Original Series",
+        sname :"Farzi",
+        link :"https://www.primevideo.com/detail/0T8LTM919PCGMZMYWGCO5I1D9B?ref_=dvm_pds_g_in_kk_s_gt_145827860537_mkw_sM71Z5AZkqH-dc&mrntrk=pcrid_658205142850_slid__pgrid_145827860537_pgeo_9146605_x__adext__ptid_kwd-1944017679309&gclid=CjwKCAjwwb6lBhBJEiwAbuVUSu2xgYRa-n8UPYGYxmzzUn_0EN5wBXjsKgjTtnABbIx9V87tXoQNexoCzVYQAvD_BwE&gclsrc=aw.ds"
+      },
+      {
+        imgsrc:"https://wallpapercave.com/w/wp6075938.jpg",
+        title :"A Amazon Prime Original Series",
+        sname :"The Family Man",
+        link :"https://www.primevideo.com/detail/0S3QYI59BAEI5KVLHCKSR91YGD/ref=atv_sr_fle_c_Tn74RA_1_1_1?sr=1-1&pageTypeIdSource=ASIN&pageTypeId=B07X443FLN&qid=1689237583499.ds"
+      },
+      {
+        imgsrc:"https://wallpapercave.com/w/wp12500096.jpg",
+        title :"A Amazon Prime Original Series",
+        sname :"Kung Fu Panda",
+        link :"https://www.primevideo.com/detail/0GM91ITQZNXR482JF9FXTQT8GZ/ref=atv_tv_hom_c_eUpBnT_brws_3_1?jic=8%7CEgRzdm9k.ds"
+      },
+      {
+        imgsrc:"https://wallpapercave.com/w/wp7285322.jpg",
+        title :"A Amazon Prime Original Series",
+        sname :"Mirzapur",
+        link :"https://www.primevideo.com/detail/0FBF7DWFYG99Y3TSNVJ4ELOLBO/ref=atv_tv_hom_c_ipeGab_brws_14_2?jic=8%7CEgRzdm9k.ds"
+      },
+      {
+        imgsrc:"https://wallpapercave.com/w/wp11176538.jpg",
+        title :"A Amazon Prime Original Series",
+        sname :"Sherlock",
+        link :"https://www.primevideo.com/detail/0Q18NAAMP9UE0PPFAHTTUE2XOL/ref=atv_dp_amz_c_Iw3Dma_1_1.ds"
+      }
+]
+
+export default SeriesData;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM  from "react-dom";
+import Card from "./Cards";
+import SeriesData from "./SeriesData";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Acard = (val) => {
+ return(
+  <Card 
+   imgsrc = {val.imgsrc}
+   title = {val.title}
+   sname ={val.sname}
+  link = {val.link}
+  />
+ );
+}
+
+ReactDOM.render(
+  <> 
+  <h1 className="heading_style">List of Top 5 WebSeries in 2023.</h1>
+  {SeriesData.map(Acard)}
+  </>
+  ,document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
